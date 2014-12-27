cbb08e9af7ef847b38c7643847a617ba2821e06d
Myhailo Talimonchuk <mychaylo.talimonchuk@iteam-pro.com>
2014-12-27 13:34:40 +0200

    Assets autoloader for actions

--- src/Event/CoreEvent.php
+++ src/Event/CoreEvent.php
@@ -13,6 +13,7 @@ namespace RearEngine\Event;
 
 use Cake\Event\EventListenerInterface;
 use Cake\Core\Plugin;
+use Cake\Utility\Inflector;
 use Cake\Core\Configure;
 use Cake\Log\Log;
 
@@ -24,6 +25,9 @@ class CoreEvent implements EventListenerInterface {
                 'callable' => 'onControllerInit',
 		        'priority' => 1
             ),
+            'View.beforeLayout' => [
+                'callable' => 'setDefaultViewAssets'
+            ]
         );
     }
 
@@ -53,4 +57,29 @@ class CoreEvent implements EventListenerInterface {
 
 		}
     }
+
+    public function setDefaultViewAssets($event){
+        $view = $event->subject();
+        $params = $view->request->params;
+        $path = Configure::read('App.webroot') . DS . '{asset}' . DS;
+        $path .= (!empty($params['prefix']) ? Inflector::underscore($params['prefix']) . DS : '');
+        $path .= Inflector::underscore($params['controller']). DS . Inflector::underscore($params['action']);
+        if (isset($params['plugin'])&&!empty($params['plugin'])){
+            $path = Plugin::path($params['plugin']). $path;
+        } else {
+            $path = ROOT . DS . $path;
+        }
+        $assetBase = $params['plugin'].'.';
+        $assetBase .= (!empty($params['prefix']) ? Inflector::underscore($params['prefix']) . '/' : '');
+        $assetBase .= Inflector::underscore($params['controller']).'/'.Inflector::underscore($params['action']);
+        $cssPath = str_replace('{asset}', str_replace('/', '', Configure::read('App.cssBaseUrl')), $path).'.css';
+        if(file_exists($cssPath)) {
+            $view->Html->css($assetBase.'.css', ['block' => true]);
+        }
+        $jsPath = str_replace('{asset}', str_replace('/', '', Configure::read('App.jsBaseUrl')), $path).'.js';
+        if(file_exists($jsPath)) {
+            $view->Html->script($assetBase.'.js', ['block' => true]);
+        }
+    }
+
 }
