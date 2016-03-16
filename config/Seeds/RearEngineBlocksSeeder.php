f12a72aa67d28e8b7ab3ea89a4828f7dba95c2c2
Denys Kyselov <denys.kyselov@gmail.com>
2016-03-16 15:03:01 +0200

    forgotten seeder

---
+++ config/Seeds/RearEngineBlocksSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class RearEngineBlocksSeeder extends AbstractSeed
+{
+    public function run()
+    {
+        $data = [
+            [
+    			'title' => 'Dashboard Top',
+    			'slug' => 'dashboard-top',
+    			'admin' => true,
+    			'cell_count' => 0,
+    		],
+    		[
+    			'title' => 'Dashboard Left Column',
+    			'slug' => 'dashboard-left',
+    			'admin' => true,
+    			'cell_count' => 0,
+    		],
+    		[
+    			'title' => 'Dashboard Right Column',
+    			'slug' => 'dashboard-right',
+    			'admin' => true,
+    			'cell_count' => 0,
+    		],
+    		[
+    			'title' => 'Dashboard Bottom',
+    			'slug' => 'dashboard-bottom',
+    			'admin' => true,
+    			'cell_count' => 0,
+    		],
+        ];
+
+        $blocks = $this->table('platform_blocks');
+        $blocks->insert($data)
+            ->save();
+    }
+}
